7aed4e91803b679ab332e30a3508fada81eabf6d
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-01-24 00:34:29 -0500

    1606

--- src/app/(days)/day2/onboarding.tsx
+++ src/app/(days)/day2/onboarding.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function OnboardingScreen() {
   return (
     <View>
       <Stack.Screen options={{ headerShown: false }} />
+
+      <FontAwesome5 name="people-arrows" size={24} color="black"/>
       <Text style={styles.title}>Track every transaction</Text>
       <Text style={styles.description}>
         Monitor your spending and contribution, ensuring every penny aligns with

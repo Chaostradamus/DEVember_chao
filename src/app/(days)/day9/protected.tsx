180d758b93147ca35cb417f37777e86fb0debdc2
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2024-04-17 20:09:33 -0400

    day 15 done

--- src/app/(days)/day9/protected.tsx
+++ src/app/(days)/day9/protected.tsx
@@ -5,8 +5,7 @@ const ProtectedScreen = () => {
   return (
     <View>
       <Text>
-        ProtectedScreen AKIA4W23YAAYKAPTBP6N
-        WpuZYq2KltwcEynbtlEWlWHcb/U/wKd+NmQo8lx1
+       
       </Text>
     </View>
   );
